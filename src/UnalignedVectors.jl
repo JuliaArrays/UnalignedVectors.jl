31db622f90e30e09428c0109d265b0075478ce53
Tim Holy <tim.holy@gmail.com>
2017-05-23 04:12:59 -0500

    Add safe indexing methods, reinterpret methods, tests

--- src/UnalignedVectors.jl
+++ src/UnalignedVectors.jl
@@ -1,15 +1,43 @@
+__precompile__(true)
+
 module UnalignedVectors
 
 export UnalignedVector, MaybeUnalignedVector, unaligned_reinterpret
 
-struct UnalignedVector{T}
+struct UnalignedVector{T} <: AbstractArray{T,1}
     a::Vector{UInt8}
+    len::Int
+
+    function (::Type{UnalignedVector{T}})(a::Vector{UInt8}) where {T}
+        len = length(a) ÷ sizeof(T)
+        len*sizeof(T) == length(a) || throw(DimensionMismatch("length(a) should be a multiple of $(sizeof(T)), got $(length(a))"))
+        new{T}(a, len)
+    end
 end
-# unsafe
-@inline Base.getindex{T}(a::UnalignedVector{T}, i) = unsafe_load(Ptr{T}(pointer(a.a)), i)
-@inline Base.length{T}(a::UnalignedVector{T}) = length(a.a) ÷ sizeof(T)
-@inline unaligned_reinterpret{T}(::Type{T}, a::Vector{UInt8}) = UnalignedVector{T}(a)
-@inline unaligned_reinterpret(::Type{UInt8}, a::Vector{UInt8}) = a
+
 MaybeUnalignedVector{T} = Union{UnalignedVector{T},Vector{T}}
+
+Base.IndexStyle(::Type{UnalignedVector{T}}) where {T} = IndexLinear()
+@inline Base.length(a::UnalignedVector) = a.len
+@inline Base.size(a) = (length(a),)
+
+@inline function Base.getindex(a::UnalignedVector{T}, i) where {T}
+    @boundscheck checkbounds(a, i)
+    unsafe_load(Ptr{T}(pointer(a.a)), i)
+end
+@inline function Base.setindex!(a::UnalignedVector{T}, val, i::Int) where {T}
+    @boundscheck checkbounds(a, i)
+    unsafe_store!(Ptr{T}(pointer(a.a)), val, i)
+end
+
+unaligned_reinterpret(::Type{T}, a::Vector{UInt8}) where {T} = UnalignedVector{T}(a)
+unaligned_reinterpret(::Type{UInt8}, a::Vector{UInt8}) = a
+
+function Base.reinterpret(::Type{T}, a::UnalignedVector, dims::Dims) where {T}
+    reshape(unaligned_reinterpret(T, a.a), dims)
+end
+function Base.reinterpret(::Type{T}, a::Base.ReshapedArray{S,N,UnalignedVector{S}}, dims::Dims) where {T,S,N}
+    reshape(unaligned_reinterpret(T, parent(a).a), dims)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,42 @@
 using UnalignedVectors
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+ENDIAN_BOM == 0x04030201 || error("tests implemented only for little endian machines")
+
+@testset "UnalignedVectors" begin
+    @test_throws DimensionMismatch UnalignedVector{UInt16}([0x01, 0x02, 0x03])
+    @test_throws ErrorException UnalignedVector{String}([0x01, 0x02])
+    @test_throws MethodError UnalignedVector{UInt16}([1, 2])
+    @test UnalignedVector{UInt16}([0x01, 0x02]) == [0x0201]
+
+    a = collect(0x01:0x14)
+    v = UnalignedVector{UInt16}(a)
+    @test IndexStyle(v) == IndexLinear()
+    @test length(v) == 10
+    @test size(v) == (10,)
+    for i = 1:10
+        @test v[i] == (2i)<<8 + 2i - 1
+    end
+    @test_throws BoundsError v[0]
+    @test_throws BoundsError v[11]
+    for i = 1:10
+        v[i] = i
+    end
+    @test v == collect(0x0001:0x000a)
+    @test a == vec(vcat((0x01:0x0a)', zeros(UInt8,1,10)))
+    @test_throws InexactError v[1] = 0xffffffff
+
+    @test unaligned_reinterpret(UInt8, a) === a
+    v = unaligned_reinterpret(Int16, a)
+    @test v == collect(1:10)
+    r = reinterpret(Int32, v, (1, 5))
+    @test size(r) == (1, 5)
+    for i = 1:5
+        @test r[1,i] == 2i*2^16 + 2i - 1
+    end
+    r2 = reinterpret(UInt16, r, (10,))
+    @test isa(r2, UnalignedVector{UInt16})
+    @test isa(reinterpret(UInt8, r, (20,)), Vector{UInt8})
+end
+
+nothing
