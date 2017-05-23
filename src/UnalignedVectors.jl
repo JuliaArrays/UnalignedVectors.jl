241a5481c451ba6bff5d23039f6f3e9dc2b5df80
Yichao Yu <yyc1992@gmail.com>
2017-05-23 02:50:19 -0500

    Define the UnalignedVector type

--- src/UnalignedVectors.jl
+++ src/UnalignedVectors.jl
@@ -1,5 +1,15 @@
 module UnalignedVectors
 
-# package code goes here
+export UnalignedVector, MaybeUnalignedVector, unaligned_reinterpret
 
-end # module
+struct UnalignedVector{T}
+    a::Vector{UInt8}
+end
+# unsafe
+@inline Base.getindex{T}(a::UnalignedVector{T}, i) = unsafe_load(Ptr{T}(pointer(a.a)), i)
+@inline Base.length{T}(a::UnalignedVector{T}) = length(a.a) ÷ sizeof(T)
+@inline unaligned_reinterpret{T}(::Type{T}, a::Vector{UInt8}) = UnalignedVector{T}(a)
+@inline unaligned_reinterpret(::Type{UInt8}, a::Vector{UInt8}) = a
+MaybeUnalignedVector{T} = Union{UnalignedVector{T},Vector{T}}
+
+end
